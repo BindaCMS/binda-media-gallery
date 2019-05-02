9a57f6d600a1ac02775c7cf5d82f20fd45b07722
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-02 12:55:05 +0200

    ADD api namespace with media controller

---
+++ app/controllers/api/media_controller.rb
@@ -0,0 +1,45 @@
+class Api::MediaController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_to do |format|
+      format.json { render json: Medium.order(created_at: :DESC) }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.json { render json: Medium.find(params[:id]) }
+    end
+  end
+
+  def create
+    respond_to do |format|
+      format.json { render json: Medium.create(medium_params) }
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      format.json { render json: Medium.destroy(params[:id]) }
+    end
+  end
+
+  def update
+    medium = Medium.find(params['id'])
+    medium.update(medium_params)
+    respond_to do |format|
+      format.json { render json: medium }
+    end
+  end
+
+  private
+
+  def medium_params
+    params.require(:event).permit(
+        :id,
+        :name,
+        :description,
+    )
+  end
+end
