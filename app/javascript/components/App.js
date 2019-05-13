a709b81729e81c49b2ac7ee49d0665f3a2a0a40e
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-13 12:52:51 +0200

    REFACTOR

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,8 @@
 // https://hibbard.eu/rails-react-crud-app/
 
 import React from 'react';
-import { Provider } from 'react-redux'
-import { BrowserRouter, Route } from 'react-router-dom';
+import {Provider} from 'react-redux'
+import {BrowserRouter, Route}  from 'react-router-dom';
 import Editor from './Editor';
 import {store} from '../store'
 
